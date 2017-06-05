e4012cbe6e916a9a12e9c0ac7fe6cec50962048a
watayan <watayan@meigaku.ac.jp>
2017-06-05 17:30:48 +0900

    左右キーでも《変数》に入ると選択状態になるようにした。

--- ui.js
+++ ui.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function add_cord(add_cord)
+function editButton(add_cord)
 {
 	var sourceTextArea = document.getElementById("sourceTextarea");
 	var pos = sourceTextArea.selectionStart;
@@ -8,14 +8,15 @@ function add_cord(add_cord)
 	var cord1 = cord.slice(0, pos);
 	var cord2 = cord.slice(pos, cord.length);
 	var re1 = /[｜| 　]*$/;
+	var re2 = /[｜| 　\n]/;
 	var add_cords = add_cord.split("\n");
 	var tab = "";
 	var array = re1.exec(cord1);
 	if(array != null) tab = array[0];
-	console.log("["+cord[pos]+"]");
-	if(cord[pos] && cord[pos] != "\n")
+//	console.log("["+cord[pos]+"]");
+	if((cord[pos] && cord[pos] != "\n") || (pos > 0 && !re2.exec(cord[pos - 1])))
 	{
-		alert("行の途中で入力支援ボタンを押してはいけません");
+		alert("この位置で入力支援ボタンを押してはいけません");
 		sourceTextArea.focus();
 		return;
 	}
@@ -30,7 +31,7 @@ function add_cord(add_cord)
 	sourceTextArea.focus();
 }
 
-function keyDown()
+function keyUp()
 {
 	var sourceTextArea = document.getElementById("sourceTextarea");
 	var pos = sourceTextArea.selectionStart;
@@ -41,21 +42,24 @@ function keyDown()
 	var re2 = /^[^》《]*》/;
 	switch(window.event.keyCode)
 	{
-	case 37: case 38: case 39: case 40:
-		if(cord[pos] == "《")
+	case 39:
+		if(pos > 0 && cord[pos - 1] == "《")
 		{
 			var match = re2.exec(cord2);
 			if(match != null)
 			{
-//				sourceTextArea.setSelectionRange(pos, pos + match[0].length + 1);
+				sourceTextArea.setSelectionRange(pos - 1, pos + match[0].length + 1);
+				return false;
 			}
 		}
-		else if(cord[pos] == "》")
+	case 37:
+		if(cord[pos] == "》")
 		{
 			var match = re1.exec(cord1);
 			if(match != null)
 			{
-//				sourceTextArea.setSelectionRange(pos - match[0].length, pos);
+				sourceTextArea.setSelectionRange(pos - match[0].length, pos + 1);
+				return false;
 			}
 		}
 		break;
@@ -63,6 +67,7 @@ function keyDown()
 //		console.log(window.event.keyCode);
 		break;
 	}
+	return true;
 }
 
 function mouseClick()
